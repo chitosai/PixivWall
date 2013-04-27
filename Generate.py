ed35e391c1b7bd51117491b7e5623198c768d5ae
TheC <i@thec.me>
2013-04-27 22:48:28 +0800

    upgrade jquery to v2.0 & add google analytics

--- Generate.py
+++ Generate.py
@@ -27,13 +27,27 @@ HTML = '''
     <div id="preload">正在预读图片</div>
     <div id="wall-wrapper"></div>
     <div id="origins">%s</div>
-    <script src="inc/jquery-1.9.1.min.js"></script>
+    <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-2.0.0.min.js"></script>
     <script src="inc/pixivwall.animations.js"></script>
     <script src="inc/pixivwall.js"></script>
     <script>
         DURATION = %s;
         DELAY = %s;
         CUBE_SIZE = %s;
+    </script>
+    <script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-24680734-2']);
+      _gaq.push(['_setDomainName', 'thec.me']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
     </script>
 </body>
 </html>
