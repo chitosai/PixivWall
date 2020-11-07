0edb5030e19898cc18182a8c48a86c10afe1f09e
TheC <i@thec.me>
2020-11-07 13:19:18 -0800

    specify image size

--- getSourceFromKonachan.js
+++ getSourceFromKonachan.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 async function main() {
-    const raw = await got('https://konachan.com/post.json?limit=50');
+    const raw = await got('https://konachan.com/post.json?limit=50&tags=width:1920%20height:1080');
     let json;
     // 检查一下数据是否是正确json，解析出错就直接退出吧，我也不想写日志啥的，太麻烦了
     json = JSON.parse(raw.body);
