0ece8f9dd88c3b2859d2ccbc7e1cef482e3f48ce
TheC <i@thec.me>
2020-11-07 12:30:18 -0800

    node get source from konachan

---
+++ getSourceFromKonachan.js
@@ -0,0 +1,15 @@
+const got = require('got');
+const fs = require('fs');
+const path = require('path');
+
+async function main() {
+    const raw = await got('https://konachan.com/post.json?limit=50');
+    let json;
+    // 检查一下数据是否是正确json，解析出错就直接退出吧，我也不想写日志啥的，太麻烦了
+    json = JSON.parse(raw.body);
+    fs.writeFile(path.join(__dirname, 'source.json'), raw.body, (err) => {
+        if (err) throw err;
+    });
+}
+
+main();
